e88cce3aae5a43ee61751a23981854c430343ed7
Nabiaaz <gtavgtavfr@gmail.com>
2026-05-28 17:50:23 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,22 @@
 export default {
   async fetch(request) {
+    const url = new URL(request.url);
     const upgradeHeader = request.headers.get("Upgrade");
-    if (!upgradeHeader || upgradeHeader !== "websocket") {
-      return new Response("OK", { status: 200 });
+
+    // Redirection vers le VLESS sur le port 10001
+    if (url.pathname === "/vless-ws") {
+      return fetch("http://79.133.42.185:10001" + url.pathname + url.search, {
+        headers: request.headers,
+      });
     }
-    const webSocketPair = new WebSocketPair();
-    const [client, server] = Object.values(webSocketPair);
-    server.accept();
-    const fetchResponse = await fetch("http://79.133.42.185:8080", {
-      headers: request.headers,
-    });
-    return new Response(null, { status: 101, webSocket: client });
+
+    // Redirection vers le Trojan sur le port 10002
+    if (url.pathname === "/trojan-ws") {
+      return fetch("http://79.133.42.185:10002" + url.pathname + url.search, {
+        headers: request.headers,
+      });
+    }
+
+    return new Response("Service actif", { status: 200 });
   },
 };
